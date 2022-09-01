776173b0fde7e4c38bebd42ca699a4fb3787c25b
Enwongo-Abasi Udomah <62923280+pezicodes@users.noreply.github.com>
2022-09-01 23:19:42 +0100

    Create LoadingScreen.cs

---
+++ 4 Dead/Assets/LoadingScreen.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+
+
+public class LoadingScreen : MonoBehaviourPunCallbacks
+{
+	public Text buttonText;
+
+	public void JoinLobby()
+    {
+        StartCoroutine(OnClickConnectButton());
+    }
+
+    void Start(){
+        MultiplayerNetworkManager.Instance.Lobby.SetActive(false);
+		MultiplayerNetworkManager.Instance.LoadingScreen.SetActive(true);
+
+		StartCoroutine(OnClickConnectButton());
+    }
+
+	IEnumerator OnClickConnectButton()
+	{		
+        yield return new WaitForSeconds(1.0f);
+		PhotonNetwork.NickName = PlayerPrefs.GetString("Username");
+		PhotonNetwork.ConnectUsingSettings();
+		buttonText.text = "Creating Lobby...";
+		MultiplayerNetworkManager.Instance.Lobby.SetActive(true);
+		MultiplayerNetworkManager.Instance.LoadingScreen.SetActive(false);
+	}
+
+	public override void OnConnectedToMaster()
+	{   
+        //StreamChatBehaviour.instance.GetOrCreateClient(nameInputFied.text);
+		PhotonNetwork.JoinLobby();
+	}
+
+	
+	// public override void OnJoinedLobby()
+	// {
+	// 	MultiplayerNetworkManager.Instance.Lobby.SetActive(true);
+	// 	MultiplayerNetworkManager.Instance.LoadingScreen.SetActive(false);
+	// }
+
+}
