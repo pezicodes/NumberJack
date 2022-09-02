a099f7c9ded10d0b84e4127a05c1b12c6bc82e2f
Enwongo-Abasi Udomah <62923280+pezicodes@users.noreply.github.com>
2022-09-02 22:26:39 +0100

    Update MultiplayerNetworkManager.cs

--- 4 Dead/Assets/MultiplayerNetworkManager.cs
+++ 4 Dead/Assets/MultiplayerNetworkManager.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using Photon.Pun;
 
 public class MultiplayerNetworkManager : MonoBehaviour
 {
