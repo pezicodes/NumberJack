16bdafad3ef031c4e68e48ce55600145d6ddcec6
Enwongo-Abasi Udomah <62923280+pezicodes@users.noreply.github.com>
2022-09-05 23:52:22 +0100

    Update IntroManager.cs

--- 4 Dead/Assets/Special/Scripts/Intro/IntroManager.cs
+++ 4 Dead/Assets/Special/Scripts/Intro/IntroManager.cs
@@ -1,23 +1,14 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-using Photon.Pun;
-
 
 public class IntroManager : ServerManager
 {
-    #region lifecycle
-    private void Awake()
-    {
-        StartCoroutine(JoinLobby());
-    }
-    #endregion
 
-    IEnumerator JoinLobby()
+    private void AfterSplashScreen()
     {
-        yield return new WaitForSeconds(1.0f);
-        AppManager.Instance.ChangeScene(AppManager.eSceneState.Lobby);
-        Debug.Log("로비로 이동합니다");
+        AppManager.Instance.ChangeScene(AppManager.eSceneState.Username);
     }
+   
+
+    
 }
