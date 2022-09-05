53f9cbed87ebb3b265efd92bac955784a9adae11
Enwongo-Abasi Udomah <62923280+pezicodes@users.noreply.github.com>
2022-09-05 23:52:24 +0100

    Update VoiceUI.cs

--- 4 Dead/Assets/Special/Scripts/Controller/VoiceUI.cs
+++ 4 Dead/Assets/Special/Scripts/Controller/VoiceUI.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-
 using Photon.Voice.PUN;
-using Photon.Voice.Unity;
 using Photon.Pun;
 using Photon.Realtime;
 
@@ -16,24 +12,33 @@ public class VoiceUI : MonoBehaviour
 
     #region Private variable
     //private Canvas canvas;
-    private PhotonView pv;
-    private PhotonVoiceView pv_voice;
-    private Player player;
+     [SerializeField] PhotonView pv;
+    [SerializeField] PhotonVoiceView pv_voice;
+    public GameObject characters;
+
     #endregion
 
-    #region LifeCycle
-    private void Awake()
-    {
-        pv = this.gameObject.GetComponentInParent<PhotonView>();
-        pv_voice = this.gameObject.GetComponent<PhotonVoiceView>();
-        //canvas = this.GetComponent<Canvas>();
-    }
+   
 
     private void Start()
     {
-       // playerName_TEXT.text = pv.Owner.NickName.ToString() + ("You");
+
+        TurnOnVoice();
 
         // 해당 Player가 방에 입장하거나 생성할 때 입력한 닉네임을 불러와 출력
+    }
+
+    
+    void TurnOnVoice(){
+
+       playerName_TEXT.text = pv.Owner.NickName;
+
+       //print(playerName_TEXT.text);
+
+       GameObject go = Instantiate(characters, RoomManager.InstanceRoomManager.RespawnSpot);
+       Text[] texts = go.GetComponentsInChildren<Text>();
+       texts[0].text = playerName_TEXT.text; //name of string
+
     }
 
     private void Update()
@@ -43,7 +48,4 @@ public class VoiceUI : MonoBehaviour
         //recording_IMG.enabled = pv_voice.IsRecording;
         // 말하고 있을 때 해당 이미지가 나오도록 출력
     }
-
-
-    #endregion
 }
