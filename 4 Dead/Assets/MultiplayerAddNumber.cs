34b4da0bbbbcf4210089cc9b22827230dd90a83d
Enwongo-Abasi Udomah <62923280+pezicodes@users.noreply.github.com>
2022-09-06 20:28:28 +0100

    Create MultiplayerAddNumber.cs

---
+++ 4 Dead/Assets/MultiplayerAddNumber.cs
@@ -0,0 +1,123 @@
+using System.Collections.Generic;
+using UnityEngine;           
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+
+public class MultiplayerAddNumber : MonoBehaviour
+{
+    #region Main Game Play Variables 
+    //public static MultiplayerManager multiPlay;
+
+    char first_opp, second_opp, third_opp, fourth_opp;
+    char first_my, second_my, third_my, fourth_my;
+    public Text Mytextbox;
+    public Button StartBtn;
+    string myEntries;
+    string OppEntries;
+    string result;
+    string Opptextbox;
+    
+    #endregion
+
+    #region GameManager's Methods
+    public void Start()
+    {
+        ColorUtility.TryParseHtmlString
+        ("#9DD300", out greenColor);
+
+        ColorUtility.TryParseHtmlString
+        ("#FFFFFF", out defaultColor);
+    
+        myEntries = Mytextbox.text;
+        myEntries.ToArray();   
+        #endregion
+    }
+
+    public void clearMemory()
+    {
+        result = "";
+        reset();
+    }
+    
+
+
+ 
+    private void Update()
+    {  
+        
+        #region Check input before play
+        if (Mytextbox.text.Length > 3)
+        {
+            StartBtn.interactable = true;
+        }
+
+        else{
+            StartBtn.interactable = false;
+        }
+
+        #endregion
+
+    }
+
+    // public void Send_Play()
+    // {   
+    //     myEntries = Mytextbox.text;
+    //     myEntries.ToArray();
+    //     OppEntries = Opptextbox;
+    //     OppEntries.ToArray();
+
+    //     // Me(player1)
+    //     first_my = myEntries[0];
+    //     second_my = myEntries[1];
+    //     third_my = myEntries[2];
+    //     fourth_my = myEntries[3];
+
+    //     // Opponent
+    //     first_opp = OppEntries[0];
+    //     second_opp = OppEntries[1];
+    //     third_opp = OppEntries[2];
+    //     fourth_opp = OppEntries[3];
+    //     #endregion
+    //     checkDeadandWounded();
+
+    // }
+
+    #region Select Tool
+    //CHEAT
+    [Space]
+    [Space]
+    [Header("Select Numbers Tool")]
+    public Button[] buttons;
+    //public Text[] texts;
+    public Color greenColor;
+    public Color defaultColor;  
+    
+    public void reset(){
+
+        clear.codeClear.cleraAll();
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].image.color = defaultColor;
+            Text btntext = buttons[i].GetComponentInChildren<Text>();
+            btntext.text = "";
+        } 
+    }
+    
+    public void decipher(){
+        //pezicodes 
+        myEntries = Mytextbox.text;
+        myEntries.ToArray();
+
+        for (int i = 0; i < myEntries.Length; i++)
+        {
+            buttons[i].image.color = greenColor;
+            Text btntext = buttons[i].GetComponentInChildren<Text>();
+            btntext.text = myEntries[i].ToString();
+           
+        }
+
+    }
+    #endregion  
+}
