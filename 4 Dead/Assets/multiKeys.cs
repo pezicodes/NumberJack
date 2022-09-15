4c855d9e3805c71c6f4880542be7287ea2f82557
Enwongo-Abasi Udomah <62923280+pezicodes@users.noreply.github.com>
2022-09-15 04:57:06 +0100

    Update multiKeys.cs

--- 4 Dead/Assets/multiKeys.cs
+++ 4 Dead/Assets/multiKeys.cs
@@ -1,4 +1,4 @@
-  using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -24,8 +24,7 @@ public class multiKeys : MonoBehaviour
     {
         spacebox.text = spacebox.text + text.text;
 
-        //MultiplayerManager.multiPlay.decipher();
-        MultiplayerAddNumber.multiplayerNumber.decipher();
+        MultiplayerManager.multiPlay.decipher();
 
         cv.interactable = false;
         if (cv.interactable == false)
